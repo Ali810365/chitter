26b1d84a09a3069be1b7d7f85066ee7d95e5dc4e
Ali810365 <mabdi@sea.yearup.org>
2023-11-10 14:18:43 -0800

    pass

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/completions', async (req, res) => {
 
     try {
        const response =  await fetch('https://api.openai.com/v1/chat/completions', options)
-       const data = await req.body
+       const data = await response.json()
        res.send(data)
        //console.log(data)
     } catch (error) {
